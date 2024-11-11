14d1293974daf0d3619bfa0c7c93c3093e9b46fd
RisingStar111 <alastark3@gmail.com>
2024-11-11 13:25:14 +0000

    Setup files
    Install Mathlib and test on an example

--- green/Green/Basic.lean
+++ green/Green/Basic.lean
@@ -1,1 +1,15 @@
-def hello := "world"
+import Mathlib.Tactic
+
+#allow_unused_tactic Lean.Parser.Tactic.done
+
+namespace Green
+
+example (p q : Prop) (hpq : p ∧ q) : q ∧ p := by
+  -- exact id (And.symm hpq)
+  ---- this lemma exists and `exact?` finds it for us
+  constructor  -- splits the `and` in the goal into two separate goals
+  · cases hpq  -- place the cursor at the end of `hpq`, wait for 💡 to appear and click on it!
+    assumption
+  · cases hpq
+    assumption
+  done
